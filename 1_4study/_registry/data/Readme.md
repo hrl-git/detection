b952c845804e2cf466a8454ffe557c78c8c7fb3b
hrl-git <85498455+hrl-git@users.noreply.github.com>
2021-12-14 10:00:55 +0900

    delete
    delete

--- 1_4study/_registry/data/Readme.md
+++
@@ -1,2 +0,0 @@
-- NTUSER.DAT: NIST Leakage Case PC Image (informant account)
-- SAM, SECURITY, SOFTWARE: NIST Leakage Case PC Image
--- 1_4study/_signatures/_makingSigtool/test_yara_step1.py
+++
@@ -1,8 +0,0 @@
-import yara
-
-rules = yara.compile(filepath='yara_msdoc.rule')
-
-matches = rules.match('example.xls')
-print(matches)
-#print(matches[1].strings)
-#help(matches[0].rule)
